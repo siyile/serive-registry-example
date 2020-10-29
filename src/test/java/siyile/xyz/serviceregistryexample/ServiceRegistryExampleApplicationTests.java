2cf251c054507f3b8a5ee64d218e18773961e625
siyile <seayahh@outlook.com>
2020-10-29 16:55:40 -0700

    implmented starter registry service

---
+++ src/test/java/siyile/xyz/serviceregistryexample/ServiceRegistryExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package siyile.xyz.serviceregistryexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
